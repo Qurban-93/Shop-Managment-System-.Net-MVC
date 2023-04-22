f302ed73073b01a3475d2ba7c622809216678690
Qurban-93 <qurban231293@gmail.com>
2023-04-22 13:51:50 +0400

    first commit

---
+++ ShopManagmentSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopManagmentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
